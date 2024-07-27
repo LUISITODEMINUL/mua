4f967c022a4193e85cc028a3e7f7da9c1f0a8e08
Bryan Villalobos <director@dysat.co>
2024-07-26 20:11:33 -0500

    [add] first commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php header('Location: https://www.bancolombia.com/centro-de-ayuda/canales/sucursal-virtual-personas'); ?> 
+
+</html>
