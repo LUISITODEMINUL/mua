a06404be782f184557ddab880cab114162758a07
Bryan Villalobos <director@dysat.co>
2024-07-26 20:54:13 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,5 @@
-<html>
-  <head>
-    <title>PHP Test</title>
-  </head>
-  <body>
-    <?php header('Location: https://www.bancolombia.com/centro-de-ayuda/canales/sucursal-virtual-personas'); ?> 
-
-</html>
+<?php
+// Redirige al usuario antes de enviar cualquier contenido HTML
+header('Location: https://sucursalpersonas.transaccionesbancolombia.com');
+exit;
+?>
